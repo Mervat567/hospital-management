a12d5aec1b9973e5093b51ceec95944d6b76d8ce
mervat <mervatgomaa34@gmail.com>
2024-04-19 23:18:28 +0200

    configuration file

--- config/app.js
+++ config/app.js
@@ -1,15 +1,20 @@
 const express=require("express")
 const app=express()
+const path=require("path")
 require('dotenv').config()
 const connection=require('./database').connection
 connection()
 
-const userRoutes=require('../routes/user/user.route')
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "../public/")));
+
+const indexRoutes=require("../routes/index.route")
+const {handleCorsPolicy}=require("../helpers/cors")
 
 app.use(express.json())
-app.use(userRoutes)
-
+app.use(indexRoutes)
+app.use(handleCorsPolicy)
 
 module.exports=app
-
 
